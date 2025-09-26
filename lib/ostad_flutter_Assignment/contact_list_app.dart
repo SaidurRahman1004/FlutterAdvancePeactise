9b71fd6e3238e7fb5c3754bcc305aa0fb22b713d
SaidurRahman1004 <saidurrahman1004@gmail.com>
2025-09-26 22:59:59 +0600

    Ostad Contact List Live Test Updated v1

--- lib/ostad_flutter_Assignment/contact_list_app.dart
+++ lib/ostad_flutter_Assignment/contact_list_app.dart
@@ -22,21 +22,35 @@ class _contactListAppState extends State<contactListApp> {
   final _nameController = TextEditingController();
   final _numberController = TextEditingController();
   final List<Map<String, String>> contacts = const [
+    {"name": "Siyam", "number": "01745-777777"},
     {"name": "Jawad", "number": "01877-777777"},
     {"name": "Ferdous", "number": "01677-777777"},
     {"name": "Hasan", "number": "01745-777777"},
     {"name": "Hasan", "number": "01745-777777"},
     {"name": "Hasan", "number": "01745-777777"},
+    {"name": "Saidur", "number": "01745-777777"}
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Contact List"),
-        centerTitle: true,
-        backgroundColor: Colors.black12,
-      ),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100), // AppBar height increase
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),  // বাঁ দিকে curve
+              topRight: Radius.circular(20), // ডান দিকে curve
+            ),
+            child: AppBar(
+              backgroundColor: Colors.blueGrey,
+              centerTitle: true,
+              title: const Text(
+                "Contact List",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Colors.white),
+              ),
+            ),
+          ),
+        ),
       body: Column(
         children: [
           Padding(padding: 
@@ -47,7 +61,14 @@ class _contactListAppState extends State<contactListApp> {
                 SizedBox(height: 10,),
                 CustomTextFieldos(controller: _numberController,keyboardType: TextInputType.phone,labelText: "Number",),
                 SizedBox(height: 10,),
-                ElevatedButton(onPressed: (){}, child: Text("Add Contact")),
+                ElevatedButton(onPressed: (){}, child: Text("Add "),style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),),
               ],
             ),
           ),
