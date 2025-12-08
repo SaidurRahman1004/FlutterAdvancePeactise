be08f0325ea0e9085a50fae56afd8bbdccde3ff5
SiyamDev <siyamdev705@gmail.com>
2025-12-08 15:25:52 +0600

    Updated Inherited Provider From Laptop 12Dec2025 3:25 pm

--- lib/ProviderWithInheritedWidgetBasic/Presentation/Screen/screen_one.dart
+++ lib/ProviderWithInheritedWidgetBasic/Presentation/Screen/screen_one.dart
@@ -4,63 +4,84 @@ import 'package:fluttert_test_code/ProviderWithInheritedWidgetBasic/Presentation
 import '../Provider/Inherite_widget.dart';
 
 class firstCounter extends StatelessWidget {
-
-   firstCounter({super.key});
-
-
-  final List<Map<String, dynamic>> actions =[
-    {
-      "label":"Increment +",
-     // "onpressed" => counterprovider.incriment();
-
-      "color":Colors.green
-    }
-
-  ];
-
+  firstCounter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final counterprovider = CounterProviderInheriteWigget.of(context).counterProvider;
+    final counterprovider = CounterProviderInheriteWigget.of(
+      context,
+    ).counterProvider;
+    final List<Map<String, dynamic>> actions = [
+      {
+        "label": "Increment +",
+        "onpressed": () => counterprovider.increment(),
+        "color": Colors.green,
+      },
+
+      {
+        "label": "Decriment -",
+        "onpressed": () => counterprovider.decrement(),
+        "color": Colors.blue,
+      },
+
+      {
+        "label": "Reset 0",
+        "onpressed": () => counterprovider.reset(),
+        "color": Colors.red,
+      },
+    ];
     final count = counterprovider.counter;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("First Counter"),
-      ),
+      appBar: AppBar(title: Text("First Counter")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("First Counter:",style: TextStyle(
-              fontSize: 20
-            ),),
+            Text("First Counter:", style: TextStyle(fontSize: 20)),
 
-            const SizedBox(height: 20,),
+            const SizedBox(height: 20),
 
-            Text("Count: ",
-              style: TextStyle(
-                fontSize: 15
-            ),),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //
-              ],
+            ListenableBuilder(
+              listenable: counterprovider,
+              builder: (BuildContext context, Widget? child) {
+                return Text(
+                    counterprovider.counter.toString(),
+                  style: TextStyle(
+                    fontSize:17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              },
 
             ),
-
-            const SizedBox(height: 20,),
-            TextButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (_)=>secondCounter()));
-            }, child: Text("go to seceond Page"))
+            const SizedBox(height: 20),
 
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: actions.map((action) {
+                return ActionChip(
+                  label: Text(action['label']),
+                  onPressed: action['onpressed'],
+                  backgroundColor: action['color'],
+                  elevation: 5,
+                );
+              }).toList(),
+            ),
 
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => secondCounter()),
+                );
+              },
+              child: Text("go to seceond Page ->"),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
--- lib/ProviderWithInheritedWidgetBasic/Presentation/Screen/screen_two.dart
+++ lib/ProviderWithInheritedWidgetBasic/Presentation/Screen/screen_two.dart
@@ -1,14 +1,67 @@
 import 'package:flutter/material.dart';
 
+import '../Provider/Inherite_widget.dart';
+
 class secondCounter extends StatelessWidget {
-  const secondCounter({super.key, });
+  secondCounter({super.key,});
 
 
   @override
   Widget build(BuildContext context) {
+    final counterprovider = CounterProviderInheriteWigget
+        .of(context)
+        .counterProvider;
+    final List<Map<String, dynamic>> actionsList = [
+      // {
+      //   "label": "Increment +",
+      //   "onpressed": () => counterprovider.increment(),
+      //   "color": Colors.green,
+      // },
+
+      {
+        "label": "Decriment -",
+        "onpressed": () => counterprovider.decrement(),
+        "color": Colors.blue,
+      },
+
+      {
+        "label": "Reset 0",
+        "onpressed": () => counterprovider.reset(),
+        "color": Colors.red,
+      },
+    ];
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Second Counter"),
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListenableBuilder(
+                listenable: counterprovider, builder: (context, child) {
+              return Text(
+                counterprovider.counter.toString(),
+                style: TextStyle(
+                  fontSize: 30,
+
+                ),
+              );
+            }),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: actionsList
+                  .map((action) =>
+                  ActionChip(label: Text(action["label"]), onPressed: action["onpressed"],backgroundColor: action['color'],))
+                  .toList(),
+            )
+
+          ],
+        ),
       ),
 
     );
